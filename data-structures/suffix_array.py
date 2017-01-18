7281a2e3c089657651c740136476a5d4712894b3
Bibek Pandey <bibek@janakitech.com>
2017-01-18 15:28:28 +0545

    change structure of project

---
+++ data-structures/suffix_array.py
@@ -0,0 +1,22 @@
+from binary_tree import BinaryTree, Node
+
+def suffix_array(string):
+    # TODO:
+    suffixes = [string[x:len(string)] for x in range(len(string))][::-1]
+    return suffixes
+
+def list_to_binary_tree(lst):
+    bintree = BinaryTree()
+    [bintree.insert(x) for x in list]
+    return bintree
+
+def suffix_tree(string):
+    bintree = BinaryTree()
+    for x in range(len(string)):
+        bintree.insert(string[x:len(string)])
+    return bintree
+
+if __name__=="__main__":
+    tree = suffix_tree("human")
+    print([x.value for x in tree.DF_traverse()])
+    print(suffix_array("human"))
