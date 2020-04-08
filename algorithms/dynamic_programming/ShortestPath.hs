2d692b3178ca7e1416373af9b12ccb963e5a8089
bewakes <bewakepandey@gmail.com>
2020-04-08 20:28:29 +0545

    Add naive cycle agnostic Shortest path

--- algorithms/dynamic_programming/ShortestPath.hs
+++ algorithms/dynamic_programming/ShortestPath.hs
@@ -9,9 +9,10 @@ data Edge t = Edge { from :: Vertex t
                    }
                    deriving Show
 
-data Graph t = Graph [Vertex t] [Edge t]
-    deriving Show
-
+data Graph t = Graph { vertices :: [Vertex t]
+                     , edges :: [Edge t]
+                     }
+                     deriving Show
 
 incomingEdges :: (Eq t) => Graph t -> Vertex t -> [Edge t]
 incomingEdges (Graph _ edges) v = filter ((v ==) . to) edges
@@ -30,7 +31,7 @@ sampleEdges = [ ("A", "B", 4)
               , ("A", "C", 2)
               , ("B", "C", 5)
               , ("D", "B", 6)
-              -- , ("B", "d", 10)
+              -- , ("B", "D", 10)
               , ("C", "E", 3)
               , ("E", "D", 4)
               , ("D", "F", 11)
@@ -48,7 +49,19 @@ shortestPathNaive g' s' t' = _shortestPathNaive g' s' t' ([], 0)
             where targetIncomings = incomingEdges g t
                   selectMin = foldl (\(minPath, minW) (vs, w) -> if minW <= w then (minPath, minW) else (vs, w)) ([], iNFINITY)
 
+-- The idea behind cycle breaking is to keep track of steps. If steps is greater
+-- than the number of vertices in graph, just ignore that path
+shortestPathNaiveCycleBreak :: (Eq a) => Graph a -> Vertex a -> Vertex a -> ([Vertex a], Int)
+shortestPathNaiveCycleBreak g' s' t' = _shortestPathNaiveCycleBreak g' s' t' ([], 0) 0
+    where _shortestPathNaiveCycleBreak g s t (path, wt) steps
+            | steps > length (vertices g) = (path, iNFINITY)
+            | s == t = (s:path, wt)
+            | otherwise = selectMin $ map (\(Edge f _ w)-> _shortestPathNaiveCycleBreak g s f (t:path, wt + w) (steps+1)) targetIncomings
+            where targetIncomings = incomingEdges g t
+                  selectMin = foldl (\(minPath, minW) (vs, w) -> if minW <= w then (minPath, minW) else (vs, w)) ([], iNFINITY)
+
 main :: IO ()
 main = do
     let graph = createGraph sampleVertices sampleEdges
-    print $ shortestPathNaive graph (Vertex "A") (Vertex "F")
+    -- print $ shortestPathNaive graph (Vertex "A") (Vertex "F")
+    print $ shortestPathNaiveCycleBreak graph (Vertex "A") (Vertex "F")
