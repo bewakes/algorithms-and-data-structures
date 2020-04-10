ef11918ec7a8e0763a515e96d65cbab09754b531
bewakes <bewakepandey@gmail.com>
2020-04-10 18:03:55 +0545

    Shortest Path with memoization: but messy

--- algorithms/dynamic_programming/ShortestPath.hs
+++ algorithms/dynamic_programming/ShortestPath.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 import qualified Data.Set as Set
+import qualified Data.Map as M
 
 newtype Vertex t = Vertex t
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
 
 data Edge t = Edge { from :: Vertex t
                    , to :: Vertex t
@@ -14,8 +17,9 @@ data Graph t = Graph { vertices :: [Vertex t]
                      }
                      deriving Show
 
+
 incomingEdges :: (Eq t) => Graph t -> Vertex t -> [Edge t]
-incomingEdges (Graph _ edges) v = filter ((v ==) . to) edges
+incomingEdges (Graph _ egs) v = filter ((v ==) . to) egs
 
 createGraph :: [String] -> [(String, String, Int)]-> Graph String
 createGraph verts eds = Graph gVertices gEdges
@@ -24,17 +28,19 @@ createGraph verts eds = Graph gVertices gEdges
           toEdge (a, b, w) = Edge (Vertex a) (Vertex b) w
 
 sampleVertices :: [String]
-sampleVertices = ["A", "B", "C", "D", "E", "F"]
+sampleVertices = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
 sampleEdges :: [(String, String, Int)]
 sampleEdges = [ ("A", "B", 4)
               , ("A", "C", 2)
               , ("B", "C", 5)
               , ("D", "B", 6)
-              -- , ("B", "D", 10)
+              , ("A", "D", 10)
+              , ("B", "D", 10)
               , ("C", "E", 3)
               , ("E", "D", 4)
               , ("D", "F", 11)
+              , ("G", "H", 11) -- Add separated edge to check if it works when there is no path
               ]
 
 iNFINITY :: Int
@@ -52,16 +58,31 @@ shortestPathNaive g' s' t' = _shortestPathNaive g' s' t' ([], 0)
 -- The idea behind cycle breaking is to keep track of steps. If steps is greater
 -- than the number of vertices in graph, just ignore that path
 shortestPathNaiveCycleBreak :: (Eq a) => Graph a -> Vertex a -> Vertex a -> ([Vertex a], Int)
-shortestPathNaiveCycleBreak g' s' t' = _shortestPathNaiveCycleBreak g' s' t' ([], 0) 0
-    where _shortestPathNaiveCycleBreak g s t (path, wt) steps
+shortestPathNaiveCycleBreak g' s' t' = _sP g' s' t' ([], 0) 0
+    where _sP g s t (path, wt) steps
             | steps > length (vertices g) = (path, iNFINITY)
             | s == t = (s:path, wt)
-            | otherwise = selectMin $ map (\(Edge f _ w)-> _shortestPathNaiveCycleBreak g s f (t:path, wt + w) (steps+1)) targetIncomings
+            | otherwise = selectMin $ map (\(Edge f _ w)-> _sP g s f (t:path, wt + w) (steps+1)) targetIncomings
             where targetIncomings = incomingEdges g t
                   selectMin = foldl (\(minPath, minW) (vs, w) -> if minW <= w then (minPath, minW) else (vs, w)) ([], iNFINITY)
+
+-- TODO: start from here, use writer or state monad
+shortestPathCycleBreakWithMemoization :: (Eq a, Ord a) => Graph a -> Vertex a -> Vertex a -> ([Vertex a], Int)
+shortestPathCycleBreakWithMemoization g' s' t' = fst $ _shortestPathMemoization g' s' t' ([], 0) (0, M.empty)
+    where _shortestPathMemoization g s t (path, wt) (steps, memo)
+            | steps > length (vertices g) = let new = M.insert (s, steps) ([], iNFINITY) memo
+                                             in (([], iNFINITY), new)
+            | s == t = let new =  M.insert (s, steps) (s:path, wt) memo
+                        in ((s:path, wt), new)
+            | otherwise = case M.lookup (s, steps) memo of
+                            Nothing -> selectMin $ map (\(Edge f _ w)-> _shortestPathMemoization g s f (t:path, wt + w) (steps+1, memo)) targetIncomings
+                            Just a -> (a, memo)
+            where selectMin = foldl (\((minPath, minW), m) ((vs, w), m') -> if minW <= w then ((minPath, minW), m) else ((vs, w), m')) (([], iNFINITY), M.empty)
+                  targetIncomings = incomingEdges g t
 
 main :: IO ()
 main = do
     let graph = createGraph sampleVertices sampleEdges
-    -- print $ shortestPathNaive graph (Vertex "A") (Vertex "F")
+    -- print $ shortestPathNaive graph (Vertex "A") (Vertex "F") -- This results infinite loop
     print $ shortestPathNaiveCycleBreak graph (Vertex "A") (Vertex "F")
+    print $ shortestPathCycleBreakWithMemoization graph (Vertex "A") (Vertex "F")
